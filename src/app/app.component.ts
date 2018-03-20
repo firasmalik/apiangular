2a6027b0d1ea6df5cd676cc4ae3f3b6655712307
firasmalik@gmail.com <firasmalik@gmail.com>
2018-03-20 15:41:27 +0530

    Longitude and Latitude

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,35 +9,32 @@ import { HttpClient} from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name:string = '';
-  age:'number';
+  location:string = '';
+  digs:'number';
+  digsii:'number';
+
   found:boolean;
 
   constructor(private httpClient:HttpClient){  }
   onNameKeyUp(event:any){
-    this.name = event.target.value;
+    this.location = event.target.value;
     this.found = false;
   }
   getProfile(){
-    //console.log(this.name);
-  //  this.httpClient.get(`https://my-json-server.typicode.com/techsithgit/json-faker-directory/profiles/?name=${this.name}`)
-   this.httpClient.get('https://maps.googleapis.com/maps/api/geocode/json?address=${this.name}')
-    .subscribe(
-      (data:any[]) => {
+      this.httpClient.get('https://maps.googleapis.com/maps/api/geocode/json?address=${this.name}')
+      .subscribe(
+                 (data:any[]) => {
 
-        console.log(data);
-        this.age=data.results[0]['geometry']['location']['lat'];
-        
+                  console.log(data);
+                   this.digs=data.results[0]['geometry']['location']['lat'];
+                   this.digsii=data.results[0]['geometry']['location']['lng'];
 
-
-
-
-            this.found = true;
+                   this.found = true;
+                         }
+                     }
+                 )
+             }
            }
-        }
-      )
-    }
-    }
 
 
 
